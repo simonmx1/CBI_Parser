99f11d841fde07ace3b0616113e0ae18eb20a78b
simonmx1 <simon@muscatello.bz.it>
2019-05-30 15:24:56 +0200

    - fixed index.php error

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * index.php
+ *
+ * Main php-file of my CBI-Standard parser
+ *
+ * @package    public
+ * @author     Simon Muscatello
+ */
+
 namespace cbi\pub;
 
 require_once __DIR__ . "/../vendor/autoload.php";
@@ -12,10 +21,11 @@ $database = 'cbi_movements';
 $user = 'root';
 $password = '';
 $file = "/home/simon/Desktop/CBI/records/06045_06045E8330_REND-CC_20190527_35109_p2_INFO.T191480305555.MOV.txt";
+$file2 = "/home/simon/Desktop/CBI/records/06045_06045E8330_REND-CC_20190524_35105_p2_INFO.T191450335574.MOV.txt";
 $testfile = "/home/simon/Desktop/test.txt";
 
 try {
-    $p = new Parser($server, $database, $user, $password, $file);
+    $p = new Parser($server, $database, $user, $password, $file2);
 } catch (Exception $e) {
 
 }
--- src/parse/Parser.php
+++ src/parse/Parser.php
@@ -1,30 +1,43 @@
 <?php
 
+/**
+ * Parse.php
+ *
+ * Manages the Parser for the CBI-standard and the upload to the Database
+ *
+ * @package    src/parse
+ * @author     Simon Muscatello
+ */
+
 namespace cbi\parse;
 
 use cbi\database\Database;
 use Exception;
 
-class Parser
-{
+class Parser {
+
     private $db;
 
-    function __construct($server, $database, $user, $password, $filename)
-    {
+    function __construct($server, $database, $user, $password, $filename) {
+
         try {
-            $this->db = (new Database($server, $database, $user, $password, "INSERT INTO cbi (m_movements, m_date) VALUES (?, ?)"));
+            $this->db = new Database($server, $database, $user, $password);
         } catch (Exception $e) {
         }
 
         $file = Cbi::fromFile($filename);
 
-        $this->db->uploadToDatabase(array(serialize($file), "2019-05-27"));
+        ///$this->db->uploadCbi(array(serialize($file), "2019-05-27"));
 
+        //var_dump($file);
+        var_dump($file->record()[4]->content()->saldoContabile());
+        var_dump($file->record()[4]->_root->_raw_record()[4]);
 
-        var_dump($file->__get("record")[4]->__get("tipoRecord"));
+        //printf("mittente: " . $file->__get("record")[0]->__get("content")->__get("mittente") . "\n");
+        //printf("ricevente: " . $file->__get("record")[0]->__get("content")->__get("ricevente") . "\n");
+        //var_dump($file->__get("record")[12]->__get("content")->__get("numeroProgressivo") . "\n");
 
         printf(var_dump($file->__get("_m_record")));
-
 
     }
 
