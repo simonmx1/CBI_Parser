007c9631d3fcd0d0abd51124b055d6c04e348975
simonmx1 <simon@muscatello.bz.it>
2019-05-30 14:45:32 +0200

    - INSERT statements

--- src/database/Database.php
+++ src/database/Database.php
@@ -9,16 +9,38 @@ class Database
 {
 
     private $conn;
-    private $stmt;
+    private $cbi_stmt;
+    private $r_testa_stmt;
+    private $saldo_i_stmt;
+    private $r_mov_stmt;
+    private $r_mov_info_stmt;
+    private $saldo_f_stmt;
+    private $r_coda_stmt;
 
-    public function __construct($server, $database, $username, $password, $stmt)
+    public function __construct($server, $database, $username, $password)
     {
 
         try {
             $this->conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $this->conn->query("SET NAMES utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->stmt = $this->conn->prepare($stmt);
+
+            //Prepare INSERT-statements for the records
+            $this->cbi_stmt = $this->conn->prepare("INSERT INTO cbi"
+                . "(m_movements, m_date) VALUES (?, ?)");
+            $this->r_testa_stmt = $this->conn->prepare("INSERT INTO record_testa "
+                . "(rt_record, rt_date, rt_cbi) VALUES (?, ?, ?)");
+            $this->saldo_i_stmt = $this->conn->prepare("INSERT INTO saldo_iniziale "
+                . "(si_record, si_date, si_cbi) VALUES (?, ?, ?)");
+            $this->r_mov_stmt = $this->conn->prepare("INSERT INTO record_movement "
+                . "(rm_record, rm_date, rm_cbi, rm_transferred) VALUES (?, ?, ?, ?)");
+            $this->r_mov_info_stmt = $this->conn->prepare("INSERT INTO record_movement_info "
+                . "(rmi_record, rmi_date, rmi_cbi, rmi_movement) VALUES (?, ?, ?, ?)");
+            $this->saldo_f_stmt = $this->conn->prepare("INSERT INTO saldo_finale "
+                . "(sf_record, sf_date, sf_cbi) VALUES (?, ?, ?)");
+            $this->r_coda_stmt = $this->conn->prepare("INSERT INTO record_coda "
+                . "(rc_record, rc_date, rc_cbi) VALUES (?, ?, ?)");
+
         } catch (PDOException $e) {
             printf($e);
         }
@@ -33,7 +55,8 @@ class Database
 
     function uploadToDatabase($upload)
     {
-        $this->stmt->execute($upload);
+
+        $this->cbi_stmt->execute($upload);
     }
 
 }
